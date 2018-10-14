04e50149f7a27d001298b7fbde650ec7ab310b71
Frances Chong <fchongg@gmail.com>
2018-10-14 00:46:18 -0700

    Added simple graph

--- gpr_simple.hs
+++ gpr_simple.hs
@@ -2,7 +2,9 @@
 -- this fit is only 1-dimensional, but the final fit will be much more
 -- I think 6-dimensional
 
-import Numeric.LinearAlgebra
+import Numeric.LinearAlgebra as NLA
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Cairo
 
 -- initialise random number generator
 -- g = getStdGen
@@ -52,7 +54,7 @@ yset = f (xset + s_noise * randomVector seed Gaussian n_train)
 -- a = vector [1..10]
 -- b = vector [3,5..21]
 -- ker_se :: Monad m => Vector R -> Vector R -> Matrix R -> m (Matrix R)
-ker_se :: Vector Double -> Vector Double -> Matrix Double -> Matrix R
+ker_se :: NLA.Vector Double -> NLA.Vector Double -> NLA.Matrix Double -> NLA.Matrix R
 ker_se a b param = do
   -- TODO will have to change this significantly for n-dimensional case
   let aa = repmat (col (toList (a^^2))) 1 (size b)
@@ -75,7 +77,7 @@ k_t = ker_se xset x_test 0.1
 Just lk = linearSolve l k_t
 y_matrix = col (toList yset)
 Just ly = linearSolve l y_matrix
-mu = (tr lk) Numeric.LinearAlgebra.<> ly
+mu = (tr lk) NLA.<> ly
 
 -- computing the variance at our test points
 k_test = ker_se x_test x_test ker_val
@@ -86,11 +88,42 @@ s2 = k_test_diag - lk_2_sum
 s = sqrt s2
 
 -- Helper functions
+-- It will produce a matrix that sums up all the columns together
+-- m is a Matrix R
+matrix_col_sum :: NLA.Matrix R -> NLA.Matrix R
 matrix_col_sum m = do
   let (_, c) = size m
   let f_list = map (\i -> sum_helper m i) [0..(c-1)]
   col f_list
 
+-- Sums up a specific column in the matrix
 sum_helper m i = do
   let col = m ¿ [i]
   sum (toList (flatten col))
+
+-- TODO: organize
+
+pairing :: [a] -> [b] -> [(a, b)]
+pairing xs ys = [ (x,y) | (x,y) <- zip xs ys]
+
+x_y_p = pairing (toList xset) (toList yset)
+x_fxt_p = pairing (toList xset) (toList (f (x_test)))
+x_mu_p = pairing (toList xset) (toList (flatten mu))
+
+mean_pred = toFile def "mean_pred.png" $ do
+  layout_title .= "Mean predictions"
+  plot (points "original data" x_y_p)
+  plot (points "x_set against x_test" x_fxt_p)
+  plot (points "x_set against average" x_mu_p)
+
+
+new_iden = diagl (replicate n_test 0.000001)
+Just prior_ch = mbChol (mTm (k_test + new_iden))
+new_l = cholSolve prior_ch (k_test + new_iden)
+-- f_prior =
+
+-- prior = toFile def "prior.png" $ do
+--   layout_title .= "Prior"
+--   plot (points "original data" x_y_p)
+--   plot (points "x_set against x_test" x_fxt_p)
+--   plot (points "x_set against average" x_mu_p)
