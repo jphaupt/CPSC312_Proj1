7f8afecf976da2b978e12dacca5eec67777521c1
Frances Chong <fchongg@gmail.com>
2018-10-14 22:39:42 -0700

    Finished logic, but need to clean up when have the time to

--- gpr_simple.hs
+++ gpr_simple.hs
@@ -89,6 +89,102 @@ lk_2_sum = matrix_col_sum (lk ^^ 2)
 s2 = k_test_diag - lk_2_sum
 s = sqrt s2
 
+
+x_y_p = pairing (toList xset) (toList yset)
+x_fxt_p = pairing (toList xset) (toList (f (x_test)))
+x_mu_p = pairing (toList xset) (toList (flatten mu))
+
+mean_pred = toFile def "mean_pred.png" $ do
+  layout_title .= "Mean predictions"
+  plot (points "original data" x_y_p)
+  plot (points "x_set against x_test" x_fxt_p)
+  plot (points "x_set against average" x_mu_p)
+
+-- draw samples from the prior
+new_iden = diagl (replicate n_test 0.000001)
+Just prior_ch = mbChol (mTm (new_iden))
+new_l = cholSolve prior_ch (k_test + new_iden)
+rand_matr = randMat n_test 10 2342432322 Gaussian
+f_prior = new_l NLA.<> rand_matr
+
+-- TODO: clean up this code if possible (tried lambda function implementation, but failed)
+prior_0_col = (toList (flatten (f_prior ¿ [0])))
+prior_1_col = (toList (flatten (f_prior ¿ [1])))
+prior_2_col = (toList (flatten (f_prior ¿ [2])))
+prior_3_col = (toList (flatten (f_prior ¿ [3])))
+prior_4_col = (toList (flatten (f_prior ¿ [4])))
+prior_5_col = (toList (flatten (f_prior ¿ [5])))
+prior_6_col = (toList (flatten (f_prior ¿ [6])))
+prior_7_col = (toList (flatten (f_prior ¿ [7])))
+prior_8_col = (toList (flatten (f_prior ¿ [8])))
+prior_9_col = (toList (flatten (f_prior ¿ [9])))
+
+prior_0 = pairing (toList x_test) prior_0_col
+prior_1 = pairing (toList x_test) prior_1_col
+prior_2 = pairing (toList x_test) prior_2_col
+prior_3 = pairing (toList x_test) prior_3_col
+prior_4 = pairing (toList x_test) prior_4_col
+prior_5 = pairing (toList x_test) prior_5_col
+prior_6 = pairing (toList x_test) prior_6_col
+prior_7 = pairing (toList x_test) prior_7_col
+prior_8 = pairing (toList x_test) prior_8_col
+prior_9 = pairing (toList x_test) prior_9_col
+
+f_prior_graph = toFile def "f_prior.png" $ do
+  layout_title .= "Ten samples from the GP prior"
+  plot (line "prior_0" [prior_0])
+  plot (line "prior_1" [prior_1])
+  plot (line "prior_2" [prior_2])
+  plot (line "prior_3" [prior_3])
+  plot (line "prior_4" [prior_4])
+  plot (line "prior_5" [prior_5])
+  plot (line "prior_6" [prior_6])
+  plot (line "prior_7" [prior_7])
+  plot (line "prior_8" [prior_8])
+  plot (line "prior_9" [prior_9])
+
+-- draw samples from the posterior
+lk_dot = (tr' lk) NLA.<> lk
+Just posterior_ch = mbChol (mTm (new_iden - lk_dot))
+new_l_post = cholSolve posterior_ch (k_test + new_iden - lk_dot)
+f_posterior = mu - (new_l_post NLA.<> rand_matr)
+
+-- TODO: clean up this code if possible (tried lambda function implementation, but failed)
+posterior_0_col = (toList (flatten (f_posterior ¿ [0])))
+posterior_1_col = (toList (flatten (f_posterior ¿ [1])))
+posterior_2_col = (toList (flatten (f_posterior ¿ [2])))
+posterior_3_col = (toList (flatten (f_posterior ¿ [3])))
+posterior_4_col = (toList (flatten (f_posterior ¿ [4])))
+posterior_5_col = (toList (flatten (f_posterior ¿ [5])))
+posterior_6_col = (toList (flatten (f_posterior ¿ [6])))
+posterior_7_col = (toList (flatten (f_posterior ¿ [7])))
+posterior_8_col = (toList (flatten (f_posterior ¿ [8])))
+posterior_9_col = (toList (flatten (f_posterior ¿ [9])))
+
+posterior_0 = pairing (toList x_test) posterior_0_col
+posterior_1 = pairing (toList x_test) posterior_1_col
+posterior_2 = pairing (toList x_test) posterior_2_col
+posterior_3 = pairing (toList x_test) posterior_3_col
+posterior_4 = pairing (toList x_test) posterior_4_col
+posterior_5 = pairing (toList x_test) posterior_5_col
+posterior_6 = pairing (toList x_test) posterior_6_col
+posterior_7 = pairing (toList x_test) posterior_7_col
+posterior_8 = pairing (toList x_test) posterior_8_col
+posterior_9 = pairing (toList x_test) posterior_9_col
+
+f_posterior_graph = toFile def "f_posterior.png" $ do
+  layout_title .= "Ten samples from the GP posterior"
+  plot (line "posterior_0" [posterior_0])
+  plot (line "posterior_1" [posterior_1])
+  plot (line "posterior_2" [posterior_2])
+  plot (line "posterior_3" [posterior_3])
+  plot (line "posterior_4" [posterior_4])
+  plot (line "posterior_5" [posterior_5])
+  plot (line "posterior_6" [posterior_6])
+  plot (line "posterior_7" [posterior_7])
+  plot (line "posterior_8" [posterior_8])
+  plot (line "posterior_9" [posterior_9])
+
 -- Helper functions
 -- It will produce a matrix that sums up all the columns together
 -- m is a Matrix R
@@ -103,44 +199,5 @@ sum_helper m i = do
   let col = m ¿ [i]
   sum (toList (flatten col))
 
--- TODO: organize
-
 pairing :: [a] -> [b] -> [(a, b)]
 pairing xs ys = [ (x,y) | (x,y) <- zip xs ys]
-
-x_y_p = pairing (toList xset) (toList yset)
-x_fxt_p = pairing (toList xset) (toList (f (x_test)))
-x_mu_p = pairing (toList xset) (toList (flatten mu))
-
-mean_pred = toFile def "mean_pred.png" $ do
-  layout_title .= "Mean predictions"
-  plot (points "original data" x_y_p)
-  plot (points "x_set against x_test" x_fxt_p)
-  plot (points "x_set against average" x_mu_p)
-
-
-new_iden = diagl (replicate n_test 0.000001)
-Just prior_ch = mbChol (mTm (k_test + new_iden))
-new_l = cholSolve prior_ch (k_test + new_iden)
-
--- testing = do
---   let vals = take 4 (mkNormals 10831452)
---   let m = (4><1) vals
---   return m
--- generate_samples n = do
---   myRandomGen <- getStdGen
---   let myRandomGen = mkStdGen 3
---   let vals = take n (normals myRandomGen)
---   (n_test><10) vals
--- myRandomGen <- getStdGen
--- myRandomGen = mkStdGen 3
--- samples = take 5 generate_samples
--- samples = take (n_test (*) 10) (normals myRandomGen)
--- m = matrix (n_test><10) samples
--- f_prior =
-
--- prior = toFile def "prior.png" $ do
---   layout_title .= "Prior"
---   plot (points "original data" x_y_p)
---   plot (points "x_set against x_test" x_fxt_p)
---   plot (points "x_set against average" x_mu_p)
